dcfdd070a747510de7d3ba55ebfbfd6d41048edc
Keiji-morita <keijim3116@gmail.com>
2022-05-23 15:01:20 +0900

    登録画面実装

---
+++ lib/login_parts/register.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class register extends StatefulWidget {
+  const register({Key? key}) : super(key: key);
+
+  @override
+  State<register> createState() => _registerState();
+}
+
+class _registerState extends State<register> {
+  bool _isObscure = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('アカウント作成'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                  labelText: 'Mail Address',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  obscureText: _isObscure,
+                  decoration: InputDecoration(
+                  labelText: 'Password',
+                  suffixIcon: IconButton(
+                      icon: Icon(_isObscure
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                      onPressed: () {
+                        setState(() {
+                          _isObscure = !_isObscure;
+                        });
+                      })),
+                ),
+              ),
+              Center(
+                child: ElevatedButton(
+                  onPressed: (){},
+                  child: Text('新規登録')
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
